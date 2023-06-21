32156d2fefbc1cde35b2c0ec2087ae02963c1232
Maxime Moge <dda119141@gmail.com>
2023-06-21 16:04:36 +0200

    added cmake list files - no compiling yet with cmake

---
+++ cmake/toolchain.cmake
@@ -0,0 +1,28 @@
+set (CROSS_COMPILE "${CMAKE_SOURCE_DIR}/../../toolchain/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-")
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER ${CROSS_COMPILE}gcc)
+
+#Don't run the linker on compiler check
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+#set(CMAKE_LINKER ${CROSS_COMPILE}ld)
+set(CMAKE_ASM_COMPILER ${CROSS_COMPILE}gcc)
+
+
+
+# Shun all C before C99
+#set(CMAKE_C_FLAGS "-std=gnu99" CACHE INTERNAL "C Compiler options")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-Map=${CMAKE_PROJECT_NAME}.map" CACHE INTERNAL "Linker options")
+
+# Debug flags
+set(CMAKE_ASM_FLAGS_DEBUG "" CACHE INTERNAL "ASM Compiler options for debug build type")
+set(CMAKE_C_FLAGS_DEBUG "-Og -g" CACHE INTERNAL "C Compiler options for debug build type")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "" CACHE INTERNAL "Linker options for debug build type")
+
+# Release flags
+set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "ASM Compiler options for release build type")
+set(CMAKE_C_FLAGS_RELEASE "-Os" CACHE INTERNAL "C Compiler options for release build type")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "" CACHE INTERNAL "Linker options for release build type")
